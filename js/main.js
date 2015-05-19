6e8411b2a661198f2e070b1451648c30b176d621
bluezebragames <bluezebragames@gmail.com>
2015-05-19 10:37:17 -0600

    Such test

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// each tile knows where it is and what its value is
+Tile = function(x,y,value){
+	this.x = x;
+	this.y = y;
+	this.value = value;
+}
+
+Tile.draw = function(){
+	Game.canvas.fillRect(0,0,50,50);
+}
+
+Game = {};
+
+Game.launch = function(){
+	Game.canvas = document.getElementById("canvas");
+	Game.tile = new Tile(0,0,2);
+	Game.tile.draw();
+}
